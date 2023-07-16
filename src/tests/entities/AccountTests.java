2dc46eff174154de221689cd97e4080737166785
giana medeiros <gianamedeiros.00510@gmail.com>
2023-07-16 17:09:24 -0300

    withdrawShouldThrowExceptionWhenInsufficientBalance

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -39,10 +39,17 @@ public class AccountTests {
 
     }
     @Test
-    public  void withdrawShouldDecreaseBalanceWhenSufficientToBalance(){
+    public  void withdrawShouldDecreaseBalanceWhenSufficientBalance(){
         Account acc = AccountFactory.createAccount(800.00);
         acc.withdraw(500.00);
 
         Assertions.assertEquals(300.00, acc.getBalance());
+    }
+    @Test
+    public void withdrawShouldThrowExceptionWhenInsufficientBalance(){
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            Account acc = AccountFactory.createAccount(800.00);
+            acc.withdraw(801.00);
+        });
     }
 }
