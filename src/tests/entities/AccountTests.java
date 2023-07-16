5b33d7f397ff40095c98407b897d81610c35c70c
giana medeiros <gianamedeiros.00510@gmail.com>
2023-07-15 21:58:26 -0300

    Test depositShouldDoNothingWhenNegativeAmount

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -6,10 +6,20 @@ import org.junit.jupiter.api.Test;
 
 public class AccountTests {
     @Test
-    public void depositShouldIncreaseBalanceWhenAmountPositive(){
+    public void depositShouldIncreaseBalanceAndDiscountFeeWhenAmountPositive(){
         double amount = 200.00;
         double expectedValue = 196.00;
         Account acc = new Account(1L,0.0);
+
+        acc.deposit(amount);
+
+        Assertions.assertEquals(expectedValue,acc.getBalance());
+    }
+    @Test
+    public  void depositShouldDoNothingWhenNegativeAmount(){
+        double expectedValue = 100.00;
+        Account acc = new Account(1L,expectedValue);
+        double amount = -200.00;
 
         acc.deposit(amount);
 
