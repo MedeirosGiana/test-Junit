1847f4913bf3bcd763785598995710305e3d394d
giana medeiros <gianamedeiros.00510@gmail.com>
2023-07-15 22:38:53 -0300

    test fullWithdrawShouldClearBalanceAndReturnFullBalance

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -26,4 +26,16 @@ public class AccountTests {
 
         Assertions.assertEquals(expectedValue,acc.getBalance());
     }
+    @Test
+    public void fullWithdrawShouldClearBalanceAndReturnFullBalance(){
+        double expectedValue = 0.0;
+        double initialBalance = 800.00;
+        Account acc = AccountFactory.createAccount(initialBalance);
+
+        double result = acc.fullWithdraw();
+
+        Assertions.assertTrue(expectedValue == acc.getBalance());
+        Assertions.assertTrue(result==initialBalance);
+
+    }
 }
